4ee7fc6749062b6e623f4bafb126aa4bd5e076af
Hawaiii <yihua25@gmail.com>
2015-11-21 23:09:44 -0500

    created main routine

---
+++ code/main.m
@@ -0,0 +1,18 @@
+% Main Routine of the 3D from Shadows System
+% Author: Mengxin Li, Yi Hua
+% Carnegie Mellon University 
+
+%% Load data
+% TODO
+
+%% Calibrate camera and desk plane
+% TODO
+
+%% Calibrate light source location
+% TODO
+
+%% 3D from scan
+% TODO
+
+%% Merge scans (if multiple scans were made)
+% TODO
