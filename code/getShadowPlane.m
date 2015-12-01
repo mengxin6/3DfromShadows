3b0fa3779755992c342486af817784db948d9de0
Hawaiii <yihua25@gmail.com>
2015-11-30 21:48:41 -0500

    selects points from line abc

--- code/getShadowPlane.m
+++ code/getShadowPlane.m
@@ -1,4 +1,4 @@
-function [shadowPlanePts] = getShadowPlane(edgeLine, lightLoc, cameraParams)
+function [shadowPlanePts] = getShadowPlane(edgeLine, lightLoc, cameraParams, xmax, ymax)
 % Calculates the shadow plane by finding 2 3d points on the intersection of 
 % shadow plane and horizontal plane.
 % Input:
@@ -6,16 +6,30 @@ function [shadowPlanePts] = getShadowPlane(edgeLine, lightLoc, cameraParams)
 %              each column is [a b c]' that specifies line ax+by+c=0
 %  cameraLoc: 3x1 vector specifying the location of light source
 %  cameraParams: a camera parameter structure
+%  xmax: size of scan image in horizontal dimension
 % Output:
 %  shadowPlanePts: 9xn matrix where n is the number of frames from the
 %                  scan; each column is [x1 y1 z1 x2 y2 z2 lx ly lz] that 
 %                  specifies 3 points on the shadow plane
 
 N = size(edgeLine2d,2);
-% TODO
-fprintf('TODO: implement point selection!\n');
 edgeLine2d = zeros(4,N);
+% find intersection of this line with the image boarder
+% intersecting with top & bottom boarder
+nonhorizontal = edgeLine(1,:)~=0;
+edgeLine2d(1,nonhorizontal) = round((-edgeLine(3,nonhorizontal)-edgeLine(2,nonhorizontal)*1)./edgeLine(1,nonhorizontal));
+edgeLine2d(2,nonhorizontal) = 1;
+edgeLine2d(3,nonhorizontal) = round((-edgeLine(3,nonhorizontal)-edgeLine(2,nonhorizontal)*ymax)./edgeLine(1,nonhorizontal));
+edgeLine2d(4,nonhorizontal) = ymax;
+% intersecting with right & left boarder
+horizontal = (edgeLine(1,:)==0 | edgeLine2d(1,:)<1 | edgeLine2d(1,:)>xmax ...
+    | edgeLine2d(3,:) < 1 | edgeLine2d(3,:) >xmax); % assuming if a=0 then b~=0
+edgeLine2d(1,horizontal) = 1;
+edgeLine2d(2,nonhorizontal) = round((-edgeLine(3,horizontal)-edgeLine(1,nonhorizontal)*1)./edgeLine(1,nonhorizontal));
+edgeLine2d(3,nonhorizontal) = xmax;
+edgeLine2d(4,nonhorizontal) = round((-edgeLine(3,horizontal)-edgeLine(1,nonhorizontal)*xmax)./edgeLine(1,nonhorizontal));
 
+% project points
 pts3d = pointsToWorld(cameraParams, mean(cameraParams.RotationMatrices,3),...
    mean(cameraParams.TranslationVectors,1), reshape(edgeLine2d,2,[])');
 shadowPlanePts = zero(9,N);
